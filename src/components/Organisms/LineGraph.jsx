d93d8f101e65bfc722ef84f460496f1f0dc702f1
whooolemilk <weekender.nyanko@gmail.com>
2022-04-16 11:52:39 +0900

    highchartsを用いてLineGraphコンポーネントを作成

---
+++ src/components/Organisms/LineGraph.jsx
@@ -0,0 +1,35 @@
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+const options = {
+  title: {
+    text: '総人口推移',
+  },
+
+  series: [
+    {
+      data: [1, 2, 3],
+    },
+  ],
+  xAxis: {
+    title: {
+      text: '年度',
+    },
+    //categories: categories,
+  },
+  yAxis: {
+    title: {
+      text: '人口数',
+    },
+  },
+};
+
+const LineGraph = () => {
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  );
+};
+
+export default LineGraph;
