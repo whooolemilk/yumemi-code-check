aa7178889253e1cc6cd2f5a5e8bafb96bc23d56d
Miyuusa <74092547+whooolemilk@users.noreply.github.com>
2022-04-16 15:16:28 +0900

    Headの設定 (#15)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>都道府県別の総人口推移グラフ</title>
+      </Head>
+      <Component {...pageProps} />
+      );
+    </>
+  );
 }
 
 export default MyApp;
