84f421c87da79457d8b7163ac1d6230b27bc7688
whooolemilk <weekender.nyanko@gmail.com>
2022-04-16 11:29:25 +0900

    RESAS_API_KEYの環境変数設定

--- src/utils/instance.js
+++ src/utils/instance.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: 'https://opendata.resas-portal.go.jp',
   timeout: 1000,
   headers: {
-    'X-API-KEY': 'bTZkeeUXm4LHtZOrbS3eB0DOd52zeUe8bgLdi4vz',
+    'X-API-KEY': process.env.NEXT_PUBLIC_RESAS_API_KEY,
   },
 });
 
