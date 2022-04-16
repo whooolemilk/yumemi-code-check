f87ed52666971789b40bbd860e3a057025bac68e
Miyuusa <74092547+whooolemilk@users.noreply.github.com>
2022-04-16 11:55:21 +0900

    highchartsを用いてLineGraphコンポーネントを作成 (#8)
    * highchartsを用いてLineGraphコンポーネントを作成
    
    * 余計なコメントの削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Title from '../components/Atoms/Title';
 import CheckItem from '../components/Organisms/CheckItem';
+import LineGraph from '../components/Organisms/LineGraph';
 
 export default function Home() {
   return (
     <>
       <Title />
       <CheckItem />
+      <LineGraph />
     </>
   );
 }
