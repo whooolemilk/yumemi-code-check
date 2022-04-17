e3ba66a89feca89c0b5fbc27cf09f6102078f83b
whooolemilk <weekender.nyanko@gmail.com>
2022-04-17 21:22:34 +0900

    存在しないcssファイルのimport削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import Title from '../components/Atoms/Title';
-import styles from '../styles/Home.module.css';
 import Graph from '../components/Organisms/Graph';
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Title />
       <Graph />
     </div>
